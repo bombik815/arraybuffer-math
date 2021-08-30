811d8394dee7c5d5468536f6f630d390964af8d3
Alexandr Rebikov <rebikovlk@mail.ru>
2021-08-30 13:30:15 +0700

    create project

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+import Daemon from './classes/daemon';
+import Magician from './classes/magician';
+
+console.log('app worked');
+
+console.log(new Daemon('demon'));
+console.log(new Magician('wizard'));
